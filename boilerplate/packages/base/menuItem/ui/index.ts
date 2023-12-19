42bcc9605356b635a3f6f1ee036b6f49393821da
fabiolapoblete <113706603+fabiolapoblete@users.noreply.github.com>
2023-12-19 13:59:32 +0100

    deployed serverless and created a menu database

---
+++ boilerplate/packages/base/menuItem/ui/index.ts
@@ -0,0 +1,17 @@
+// import './style.scss';
+// import { ReactNode } from 'react';
+// import { StyleTypes } from '@zocom/types';
+
+interface MenuItem {
+    title: string,
+    desc: string,
+    price: number,
+    category: string
+    ingredients: [],
+} 
+
+export const MenuItem = () => {
+    return (
+        <article><article/>
+    )
+}
