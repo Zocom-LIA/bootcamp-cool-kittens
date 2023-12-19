d0cd17e4503e672a5eda169dde40bdb577e8ea10
fabiolapoblete <113706603+fabiolapoblete@users.noreply.github.com>
2023-12-19 12:35:18 +0100

    Changed title on page

--- boilerplate/packages/core/router/data/index.tsx
+++ boilerplate/packages/core/router/data/index.tsx
@@ -3,19 +3,23 @@ import {
     RouterProvider,
   } from "react-router-dom";
   
-  import { LandingPage } from "@zocom/landing-page";
-  
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <LandingPage />,
-    },
-    {
-      path: "*",
-      element: <p>Page Not Found</p>,
-    },
-  ]);
-  
-  export const AppRoutes = () => {
-    return <RouterProvider router={router} />;
-  };
+import { LandingPage } from "@zocom/landing-page";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "/kitchen",
+    // element: <KitchenPage />
+  },
+  {
+    path: "*",
+    element: <p>Page Not Found</p>,
+  },
+]);
+
+export const AppRoutes = () => {
+  return <RouterProvider router={router} />;
+};
