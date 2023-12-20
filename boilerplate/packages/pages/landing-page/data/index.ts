f6163537bc45cfe01420f62c70d2e737f1e0f312
fabiolapoblete <113706603+fabiolapoblete@users.noreply.github.com>
2023-12-20 14:44:59 +0100

    Rendering of menu items by category.

--- boilerplate/packages/pages/landing-page/data/index.ts
+++ boilerplate/packages/pages/landing-page/data/index.ts
@@ -1,15 +1,51 @@
+// Fixa typescript
+
 export const menuData = () => {
   return {
-    async fetchMenu (): Promise<any> { // vilken type ska det vara. 
+    async fetchMenu(category: string) {
       try {
-        const URL = "https://s1ev3z9454.execute-api.eu-north-1.amazonaws.com/api/menu"
+        const URL = `https://s1ev3z9454.execute-api.eu-north-1.amazonaws.com/api/menu/${category}`;
         const response = await fetch(URL)
         return await response.json()
       } catch (error) {
-        console.error(error);
+        console.log(error);
+        
       }
     }
   }
-}
+};
+
+// export const menuData = () => {
+//   const categories = ["wonton", "dip"];
+  
+//   return {
+//     async fetchMenu(): Promise<any> {
+//       try {
+//         const filteredMenuItems = [];
+
+//         await Promise.all(
+//           categories.map(async (category) => {
+//             const categoryData = {
+//               category: "",
+//               items: []
+//             }
+//             const URL = `https://s1ev3z9454.execute-api.eu-north-1.amazonaws.com/api/menu/${category}`;
+//             const response = await fetch(URL);
+//             const filteredData = await response.json();
+//             categoryData.category = category
+//             categoryData.items = filteredData
+
+//             filteredMenuItems.push(categoryData);
+//           })
+//         );
+//         return filteredMenuItems;
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     },
+//   };
+// };
+
+
 
 
--- boilerplate/packages/pages/landing-page/ui/index.tsx
+++ boilerplate/packages/pages/landing-page/ui/index.tsx
@@ -16,31 +16,46 @@ type MenuItem = {
 
 export const LandingPage = () => {
 
-    const [menu, setMenu] = useState<MenuItem[]>([])
+    const [wontonMenu, setWontonMenu] = useState<MenuItem[]>([])
+    const [dipMenu, setDipMenu]= useState<MenuItem[]>([])
     const {fetchMenu} = menuData();
-    
+
     useEffect(() => {
-        async function handleFetchMenu() {
-            const data = await fetchMenu()
-            const menu = data.menu
-            setMenu(menu ? menu: null)
-            console.log(menu);
-            
+        async function handleFetchWontonMenu() {
+            const data = await fetchMenu("wonton")
+            const wontonMenu = data.filteredMenuItems
+            setWontonMenu(wontonMenu? wontonMenu: null)
+            console.log("Wonton", wontonMenu)
         }
-        handleFetchMenu();
+        async function handleFetchDipMenu() {
+            const data = await fetchMenu("dip")
+            const dipMenu = data.filteredMenuItems
+            setDipMenu(dipMenu? dipMenu: null)
+            console.log("Dip", dipMenu)
+        }
+        handleFetchWontonMenu();
+        handleFetchDipMenu();
     }, []);
 
-    const sortedMenu = menu.sort((a, b) => a.id - b.id);
+    // const sortedMenu = menu.sort((a, b) => a.id - b.id);
 
     return (
         <main>
-            <h2>Meny</h2>
-            <section>
-                {sortedMenu && sortedMenu.map((menuItem) => (
-                    // <MenuItem key={menuItem.id} data={menuItem}/>
-                    <MenuItem key={menuItem.id} title={menuItem.title} price={menuItem.price} desc={menuItem.desc}/>
-                ))}
-            </section>
+          <h2>Meny</h2>
+          <section>
+            {
+                wontonMenu && wontonMenu.map((wonton)=> (
+                    <MenuItem key={wonton.id} title={wonton.title} price={wonton.price} desc={wonton.desc}/>
+                )) 
+            }
+          </section>
+          <section>
+            {
+                dipMenu && dipMenu.map((dip)=> (
+                    <MenuItem key={dip.id} title={dip.title} price={dip.price} desc={dip.desc}/>
+                ))  
+            }
+          </section>
         </main>
-    )
+      );
 }
