e12056779cd99559260bd94e44109930f43faeba
fabiolapoblete <113706603+fabiolapoblete@users.noreply.github.com>
2023-12-22 13:24:51 +0100

    added simple design to qty buttons

--- boilerplate/packages/base/qtyButton/ui/index.tsx
+++ boilerplate/packages/base/qtyButton/ui/index.tsx
@@ -1,4 +1,5 @@
 import {motion} from "framer-motion"
+import './style.scss'
 
 type QtyButtonProps = {
     title: string
@@ -8,7 +9,7 @@ type QtyButtonProps = {
 
 export const QtyButton = ({title, action, disabled}: QtyButtonProps) => {
     return (
-        <motion.button className="" onClick = {action} disabled={disabled}>
+        <motion.button className="button--quantity" onClick = {action} disabled={disabled}>
             {title}
         </motion.button>
     )
