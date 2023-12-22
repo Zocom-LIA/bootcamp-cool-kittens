88a84df626a7af62cbc0eec2fbe68c3b45cfaba1
fabiolapoblete <113706603+fabiolapoblete@users.noreply.github.com>
2023-12-22 15:32:47 +0100

    Changed structure in cart modal, added simple styling

--- boilerplate/packages/base/cartModal/ui/index.tsx
+++ boilerplate/packages/base/cartModal/ui/index.tsx
@@ -2,29 +2,36 @@ import { useContext, useEffect, useState } from "react"
 import AppContext from "../../../core/context/AppContext"
 import { CartItem } from '@zocom/cart-item'
 import { PrimaryButton } from '@zocom/primary-button'
+import { motion, AnimatePresence } from "framer-motion";
+import './style.scss'
+import { CartIcon } from '../../../pages/landing-page/ui/cartIcon'
 
-type CartModalProps = {
-    isOpen: boolean
-    closeModal: () => void
-}
+// type CartModalProps = {
+//     isOpen: boolean
+//     closeModal: () => void
+// }
 
-export const CartModal = ({isOpen, closeModal}: CartModalProps) => {
+// export const CartModal = ({isOpen, closeModal}: CartModalProps) => {
+export const CartModal = () => {
     const {cart} = useContext(AppContext) //add setCart later
     const [totalPrice, setTotalPrice] = useState<number>(0)
+    const [cartQty, setCartQty] = useState<number>(0)
+    const [cartModalOpen, setCartModalOpen] = useState<boolean>(false);
 
-    useEffect(() => {
-        const handleKeyDown = (e: KeyboardEvent) => {
-            if (e.key === "Escape") {
-            closeModal();
-            }
-        };
 
-        if (isOpen) {
-            window.addEventListener("keydown", handleKeyDown);
-        }
+    // useEffect(() => {
+    //     const handleKeyDown = (e: KeyboardEvent) => {
+    //         if (e.key === "Escape") {
+    //         closeModal();
+    //         }
+    //     };
 
-        return () => window.removeEventListener("keydown", handleKeyDown);
-    }, [isOpen, closeModal]);
+    //     if (isOpen) {
+    //         window.addEventListener("keydown", handleKeyDown);
+    //     }
+
+    //     return () => window.removeEventListener("keydown", handleKeyDown);
+    // }, [isOpen, closeModal]);
 
     const calcTotalPrice = () => {
         let price = 0
@@ -38,12 +45,47 @@ export const CartModal = ({isOpen, closeModal}: CartModalProps) => {
         calcTotalPrice()
     }, [cart])
 
+    // Updates the cart quantity when cart is updated
+    useEffect(() => {
+        if (cart) {
+        let qty = 0;
+
+        cart.forEach((item) => {
+            qty += item.quantity;
+        });
+
+        setCartQty(qty);
+        }
+    }, [cart]);
+
     return (
-        <>
-            {isOpen && (
-                <main>
-                {/* <main onClick={closeModal}></main> */}
-                    {
+        <main>
+            <div className="cart__icon" onClick={()=>setCartModalOpen(!cartModalOpen)}>
+                {CartIcon}
+                {
+                    cartQty > 0 && (
+                        <span className='cart__qty'>{cartQty}</span>
+                    )
+                }
+            </div>
+
+            <AnimatePresence>
+                {cartModalOpen && (
+                    <motion.main             
+                    className="cart__container"
+                    initial={{ height: 0, opacity: 0 }}
+                    animate={{ height: "100vh", opacity: 1 }}
+                    transition={{ duration: 0.5 }}
+                    exit={{
+                      opacity: 0,
+                      height: 0,
+                      transition: {
+                        ease: "easeInOut",
+                        duration: 0.3,
+                        delay: 1.2,
+                      },
+                    }}>
+                        {
                         cart.length > 0 ? 
                         (<section>
                             {cart.map((cartItem)=> (
@@ -51,12 +93,13 @@ export const CartModal = ({isOpen, closeModal}: CartModalProps) => {
                             ))}
                         </section>)
                         :(<section>Your cart is empty</section>)
-                    }
-                    <section>Total price: {totalPrice}</section>
-                    {/*Need to add an action to primary button -> send order*/}
-                    <PrimaryButton title="Take my money!" disabled={cart.length < 1}/>
-                </main>
-            )}
-        </>
+                        }
+                        <section>Total price: {totalPrice}</section>
+                        {/*Need to add an action to primary button -> send order*/}
+                        <PrimaryButton title="Take my money!" disabled={cart.length < 1}/>  
+                    </motion.main>
+                )}
+            </AnimatePresence>
+        </main>
     )
 }
--- boilerplate/packages/pages/landing-page/ui/index.tsx
+++ boilerplate/packages/pages/landing-page/ui/index.tsx
@@ -1,10 +1,8 @@
-import { useEffect, useState, useContext } from 'react';
-import AppContext from "../../../core/context/AppContext"
+import { useEffect, useState } from 'react';
 import { CartModal } from '@zocom/cart-modal'
 import { MenuItem } from '@zocom/menu-item'
 import { DipOption } from '@zocom/dip-option'
 import { menuData } from '..';
-import { CartIcon } from './cartIcon'
 import './style.scss';
 
 type MenuItem = {
@@ -17,9 +15,6 @@ type MenuItem = {
 } 
 
 export const LandingPage = () => {
-    const {cart} = useContext(AppContext)
-    const [cartQty, setCartQty] = useState<number>(0)
-    const [cartModalOpen, setCartModalOpen] = useState<boolean>(false);
     const [wontonMenu, setWontonMenu] = useState<MenuItem[]>([])
     const [dipMenu, setDipMenu]= useState<MenuItem[]>([])
     const {fetchMenu} = menuData();
@@ -42,57 +37,35 @@ export const LandingPage = () => {
         handleFetchDipMenu();
     }, []);
 
-    // Updates the cart quantity when cart is updated
-    useEffect(() => {
-        if (cart) {
-        let qty = 0;
-
-        cart.forEach((item) => {
-            qty += item.quantity;
-        });
-
-        setCartQty(qty);
-        }
-    }, [cart]);
-
     // const sortedMenu = menu.sort((a, b) => a.id - b.id);
 
     return (
         <div className='landing-page'>
-            {
-                cartModalOpen ? (<CartModal isOpen={cartModalOpen} closeModal = {()=> setCartModalOpen(false)}/>):(<main>
-                    <div onClick={() => setCartModalOpen(true)}>
-                        {CartIcon }
-                    </div>
+            <header>
+                <CartModal />
+            </header>
+            <main>                    
+                <h2>Meny</h2>
+                <section>
                     {
-                        cartQty > 0 && (
-                            <span className='cart__qty'>{cartQty}</span>
-                        )
+                        wontonMenu && wontonMenu.map((wonton)=> (
+                            <MenuItem key={wonton.id} wonton={wonton}/>
+                        )) 
                     }
-                    
-                    <h2>Meny</h2>
+                </section>
+                <section>
                     <section>
-                        {
-                            wontonMenu && wontonMenu.map((wonton)=> (
-                                <MenuItem key={wonton.id} wonton={wonton}/>
-                            )) 
-                        }
+                        <h3>Dipsås</h3>
+                        {/* <hr/> */}
+                        <h3>19 sek</h3>
                     </section>
-                    <section>
-                        <section>
-                            <h3>Dipsås</h3>
-                            {/* <hr/> */}
-                            <h3>19 sek</h3>
-                        </section>
-                        {
-                            dipMenu && dipMenu.map((dip)=> (
-                                <DipOption key={dip.id} dip={dip}/>
-                            ))  
-                        }
-                    </section>
-                </main>
-                )
-            }
-            </div>         
+                    {
+                        dipMenu && dipMenu.map((dip)=> (
+                            <DipOption key={dip.id} dip={dip}/>
+                        ))  
+                    }
+                </section>
+            </main>
+        </div>
     );
 }
