5f2481f2fd96349f69681be8b544640e2fcbc478
Mercenaress <linden9707@gmail.com>
2024-01-04 14:58:34 +0100

    removed unused import

--- client/packages/base/dipOption/ui/index.tsx
+++ client/packages/base/dipOption/ui/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useContext } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { AppContext } from "@zocom/app-context";
 import './style.scss';
 
