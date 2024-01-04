706b425f9a19c471ba664c543adef64626be2d0d
Mercenaress <linden9707@gmail.com>
2024-01-04 11:24:26 +0100

    Styling edits confirmation

--- client/packages/pages/confirmation-page/ui/index.tsx
+++ client/packages/pages/confirmation-page/ui/index.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Header } from '@zocom/page-header';
-import { Link } from 'react-router-dom';
-import { FoodBoxIcon } from '../../../core/assets/foodBoxIcon'
+// import { Link } from 'react-router-dom';
 import { PrimaryButton } from '@zocom/primary-button'
 import './style.scss';
 
@@ -13,24 +12,24 @@ export const ConfirmationPage = () => {
 
 
   return (
-    <div className='confirmation-page'>
+    <section className='confirmation-page'>
       <Header />
-      {FoodBoxIcon}
-      <main className='menu-wrap'>
-        <h2 className='menu-title'>DINA WONTONS TILLAGAS</h2>
+      <main className='confirmation-wrap'>
+        <img src="./public/assets/boxtop 1.png" alt="" />
+        <h2 className='title'>DINA WONTONS TILLAGAS!</h2>
         <section>
+          <p className='eta-text'>ETA {eta} min</p>
+          <p className='order-id'>#86590238052
+            {/* {orderId} */}
+            </p> 
         </section>
-        <section className='eta-timer'>
-          <p>ETA: {eta} minuter</p>
-          {/* <p>Order ID: {orderId}</p>  */}
-        </section>
-        <section className='action'>
+        <section className='button__container'>
         <PrimaryButton className='black-bg' title='Beställ mer' />
         <PrimaryButton className='no-bg' title='Se Kvitto' />
         {/* <Link to='/bestall-mer' className='order'>Beställ mer</Link> // 
         <Link to='/se-kvitto' className='receipt'>Se kvitto</Link> // onlick */}
         </section>
       </main>
-    </div>
+    </section>
   );
 };
