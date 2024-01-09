d2ad3c511b432ac9a927427b672d1092da5f91aa
Mercenaress <linden9707@gmail.com>
2024-01-09 10:45:47 +0100

    Small edits

--- client/packages/base/dipOption/ui/index.tsx
+++ client/packages/base/dipOption/ui/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useContext } from "react";
+import { useContext } from "react";
 import { motion } from "framer-motion";
 import { AppContext } from "@zocom/app-context";
 import './style.scss';
