ced1e9eeaa2352610484c94a4e31f376a95cf6b1
AntonLindskog96 <lindskog96@live.se>
2024-01-10 13:42:41 +0100

    fixed fetch to secret key

--- client/packages/pages/kitchen-page/data/index.ts
+++ client/packages/pages/kitchen-page/data/index.ts
@@ -6,8 +6,16 @@ export const filteredOrderData = () => {
   return {
     async fetchFilteredOrders(status: string){
       try {
-        const URL = `https://s1ev3z9454.execute-api.eu-north-1.amazonaws.com/api/filterOrders/${status}?timeStamp=${todaysDate}`;
-        const response = await fetch(URL)
+        const BASE_URL = import.meta.env.VITE_API_BASE_URL
+        const API_ENDPOINT = `/filterOrders/${status}?timeStamp=${todaysDate}`
+        const API_URL = BASE_URL + API_ENDPOINT
+
+        const response = await fetch(API_URL, {
+          method: 'GET',
+          headers: {
+            authorization: `${import.meta.env.VITE_AUTH_API_KEY}`
+          }
+        })
         return await response.json()
       } catch (error) {
         console.error(error, `Failed to fetch ${status} orders`);
