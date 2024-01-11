260061af4ff7305f2a4566db8b89387afe81b140
fabiolapoblete <113706603+fabiolapoblete@users.noreply.github.com>
2024-01-11 16:01:39 +0100

    Fixed clip/fill-rule and changed deliveryTime

--- backend/functions/putOrder/calculateDeliveryTime.js
+++ backend/functions/putOrder/calculateDeliveryTime.js
@@ -1,15 +1,12 @@
 export const calculateDeliveryTime = (orderItems) => {
-  // Define delivery time parameters
-  const preparationTimePerMeal = 5; // in minutes
-  const averageDeliveryTime = 15; // in minutes
+  const preparationTimePerMeal = 1; // in minutes
+  const averageDeliveryTime = 5; // in minutes
 
-  // Calculate total preparation time based on the quantity of meals in the order
   let totalPreparationTime = 0;
   orderItems.forEach((item) => {
     totalPreparationTime += item.quantity * preparationTimePerMeal;
   });
 
-  // Calculate the estimated delivery time
   const currentTimestamp = new Date();
   const estimatedDeliveryTime = new Date(
     currentTimestamp.getTime() +
